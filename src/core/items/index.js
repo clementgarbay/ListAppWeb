96fb70255a1d5ce91c7d98c4005cbb5d3ded3a4a
Clément Garbay <clementgarbay@gmail.com>
2016-12-23 22:12:08 +0100

    Update items dispatch functions

--- src/core/items/index.js
+++ src/core/items/index.js
@@ -1,5 +1,39 @@
-import * as itemsActions from './actions';
+import { FirebaseStorage } from '../storage/firebase-storage'
+import * as itemsActions from './actions'
+import { Item } from '../models/item'
 
-export { itemsActions }
-export * from './action-types'
-export { listReducer } from './reducer'
+
+const storage = new FirebaseStorage(Item, {
+  onCreate: itemsActions.createItemSuccess,
+  onUpdate: itemsActions.updateItemSuccess,
+  onDelete: itemsActions.deleteItemSuccess,
+  onLoad: itemsActions.loadItemsSuccess
+}, 'items')
+
+function createItem(name: string): Function {
+  return (dispatch: Function) => {
+    const item = new Item({name: name})
+    storage.create(item)
+      .catch((error: *): void => dispatch(itemsActions.createItemError(error)))
+  }
+}
+
+function updateItem(key: string, values: {}): Function {
+  return (dispatch: Function) => {
+    storage.update(key, values)
+      .catch((error: *): void => dispatch(itemsActions.updateItemError(error)))
+  }
+}
+
+function loadItems(): Function {
+  return (dispatch: Function) => {
+    storage.subscribe(dispatch)
+  }
+}
+
+function unloadItems(): {} {
+  storage.unsubscribe()
+  return itemsActions.unloadItemsSuccess()
+}
+
+export default { createItem, updateItem, loadItems, unloadItems }
