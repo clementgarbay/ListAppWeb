5c1bf211919aff3135fbd5f8df4ce05017171575
Clément Garbay <clementgarbay@gmail.com>
2016-12-27 00:42:30 +0100

    Adding lists reducer

--- src/core/reducers.js
+++ src/core/reducers.js
@@ -1,8 +1,10 @@
 import { routerReducer } from 'react-router-redux'
 import { combineReducers } from 'redux'
 import { itemsReducer } from './items/reducer'
+import { listsReducer } from './lists/reducer'
 
 export default combineReducers({
   routing: routerReducer,
-  items: itemsReducer
+  items: itemsReducer,
+  lists: listsReducer
 })
