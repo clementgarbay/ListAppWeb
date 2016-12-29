dd555f53506abc3f5e06f074a9b1072a7bfc59ca
Clément Garbay <clementgarbay@gmail.com>
2016-12-29 23:26:19 +0100

    Change firebase configuration to use a dev database (dev firebase app)

--- src/core/firebase/firebase.js
+++ src/core/firebase/firebase.js
@@ -1,11 +1,19 @@
 import firebase from 'firebase'
 
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCDJPOYj7IurZVvH-y4D7PPYMk9vug1xLE',
+//   authDomain: 'listapp-5effa.firebaseapp.com',
+//   databaseURL: 'https://listapp-5effa.firebaseio.com',
+//   storageBucket: 'listapp-5effa.appspot.com',
+//   messagingSenderId: '843938428073'
+// }
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyCDJPOYj7IurZVvH-y4D7PPYMk9vug1xLE',
-  authDomain: 'listapp-5effa.firebaseapp.com',
-  databaseURL: 'https://listapp-5effa.firebaseio.com',
-  storageBucket: 'listapp-5effa.appspot.com',
-  messagingSenderId: '843938428073'
+  apiKey: 'AIzaSyAdDFQZG402cleJM9dJleP4dLERB0rxoaQ',
+  authDomain: 'listapp-dev.firebaseapp.com',
+  databaseURL: 'https://listapp-dev.firebaseio.com',
+  storageBucket: 'listapp-dev.appspot.com',
+  messagingSenderId: '790087396038'
 }
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig)
